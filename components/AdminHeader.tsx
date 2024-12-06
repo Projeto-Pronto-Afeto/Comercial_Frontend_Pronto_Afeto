fc90589bc3c1cb2014ae4cea7d4748851d0710b0
Rebeca Aguiar <beca.cout@gmail.com>
2024-12-05 23:32:07 -0300

    todo: refine sidebar and header

--- components/AdminHeader.tsx
+++ components/AdminHeader.tsx
@@ -1,45 +1,50 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "@/components/ui/popover";
-  import Image from "next/image";
-  import Link from "next/link";
-  import React from "react";
-  import { LuBell, LuSearch, LuSettings, LuSettings2 } from "react-icons/lu";
-  import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb";
-  import { Separator } from "./ui/separator";
-  import { SidebarTrigger } from "./ui/sidebar";
-  const AdminHeader = () => {
-    return (
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-    );
-  };
-  
-  export default AdminHeader;
-  
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "./ui/separator";
+import { SidebarTrigger } from "./ui/sidebar";
+
+const AdminHeader = () => {
+  const pathname = usePathname();
+
+  const pathnames = pathname.split("/").filter((x) => x);
+
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            {pathnames.map((name, index) => {
+              const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+              const isLast = index === pathnames.length - 1;
+              return (
+                <React.Fragment key={name}>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem className={isLast ? "hidden md:block" : ""}>
+                    <BreadcrumbLink href={routeTo}>
+                      {name.charAt(0).toUpperCase() + name.slice(1)}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </React.Fragment>
+              );
+            })}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </header>
+  );
+};
+
+export default AdminHeader;

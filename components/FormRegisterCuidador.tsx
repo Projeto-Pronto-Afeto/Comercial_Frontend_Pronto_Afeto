f1e0c229feda912d083cf89897e461e2ab374d99
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2024-12-10 14:44:54 -0300

    corrigir input select

--- components/FormRegisterCuidador.tsx
+++ components/FormRegisterCuidador.tsx
@@ -1,65 +1,73 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { cuidadorSchema } from "@/lib/validation";
+import CustomFormField, { FormFieldType } from "./main/inputs/CustomFormField";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
+import { SelectItem } from "@radix-ui/react-select";
+import { useState } from "react";
 
 export function SignupForm() {
-  const router = useRouter();
+  const form = useForm<z.infer<typeof cuidadorSchema>>({
+    resolver: zodResolver(cuidadorSchema),
+  });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const { handleSubmit, control } = form;
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
-    const formData = new FormData(e.currentTarget);
+  const Escolaridades = ["NÍVEL_MÉDIO", "NÍVEL_SUPERIOR", "Viúvo(a)"];
 
-    //formatar os campos que precisam de colchetes -> enquanto não resolver bug do servidor
-    const formatWithBrackets = (fieldName: string) => {
-      const value = formData.get(fieldName);
-      return value ? `[${value}]` : `[string]`;
+
+  const onSubmit = async (data: z.infer<typeof cuidadorSchema>) => {
+    const cuidador = {
+      nome: data.nome,
+      nomeApresentacao: data.nomeApresentacao,
+      telefone: data.telefone,
+      rg: data.rg,
+      rua: data.rua,
+      numero: data.numero,
+      bairro: data.bairro,
+      complemento: data.complemento,
+      cep: data.cep,
+      cidade: data.cidade,
+      estado: data.estado,
+      nomePai: data.nomePai,
+      nomeMae: data.nomeMae,
+      dataNascimento: data.dataNascimento,
+      peso: parseFloat(data.peso.toString()) || 0,
+      altura: parseFloat(data.altura.toString()) || 0,
+      escolaridade: data.escolaridade || "NÍVEL_MÉDIO",
+      titulacao: data.titulacao,
+      tempoExperiencia: parseInt(data.tempoExperiencia.toString(), 10) || 0,
+      habilidades: [{ nome: data.habilidades }],
+      experiencias: [{ nome: data.experiencias }],
+      apresentacao: data.apresentacao,
     };
 
-    // Ajustar os campos específicos
-    formData.set(
-      "experiencias",
-      JSON.stringify([{ nome: formData.get("experiencias") }])
-    );
-    formData.set(
-      "habilidades",
-      JSON.stringify([{ nome: formData.get("habilidades") }])
-    );
-    formData.set("nome", formatWithBrackets("nome"));
-    formData.set("nomeApresentacao", formatWithBrackets("nomeApresentacao"));
-    formData.set("cidade", formatWithBrackets("cidade"));
-    formData.set("bairro", formatWithBrackets("bairro"));
-    formData.set("titulacao", formatWithBrackets("titulacao"));
-    formData.set("estado", formatWithBrackets("estado"));
-    formData.set("complemento", formatWithBrackets("complemento"));
-    formData.set("apresentacao", formatWithBrackets("apresentacao"));
-    formData.set("cep", formatWithBrackets("cep"));
-    formData.set("rg", formatWithBrackets("rg"));
-    formData.set("nomeMae", formatWithBrackets("nomeMae"));
-    formData.set("numero", formatWithBrackets("numero"));
-    formData.set("rua", formatWithBrackets("rua"));
-    formData.set("nomePai", formatWithBrackets("nomePai"));
+    const formData = new FormData();
 
-    console.log("Dados formatados:");
-    Array.from(formData.entries()).forEach(([key, value]) => {
-      console.log(`${key}: ${value}`);
-    });
+    // o objeto cuidador vai como JSON
+    formData.append(
+      "cuidador",
+      new Blob([JSON.stringify(cuidador)], { type: "application/json" })
+    );
+
+    if (imageFile) {
+      formData.append("image", imageFile);
+    }
 
     try {
       const response = await fetch("http://localhost:8080/api/cuidadores/v1", {
         method: "POST",
-        headers: {
-          Accept: "*/*",
-        },
         body: formData,
       });
 
       if (response.ok) {
         alert("Cadastro realizado com sucesso!");
-        router.push("/sucesso");
       } else {
         const errorData = await response.json();
-        console.error(`Erro: ${errorData.message}`);
+        console.error("Erro ao enviar dados:", errorData);
         alert(`Erro: ${errorData.message}`);
       }
     } catch (error) {
@@ -69,167 +77,175 @@ export function SignupForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full max-w-lg flex flex-col">
-      <div className="">
+    <FormProvider {...form}>
 
-        <div className=" flex flex-col">
-          <input
-            type="file"
-            name="image"
-            accept="image/*"
-            className="border border-gray-300 p-2 m-2"
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg flex flex-col gap-4">
+        <input
+          type="file"
+          name="image"
+          accept="image/*"
+          className="border border-gray-300 p-2 m-2"
+          onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              setImageFile(e.target.files[0]);
+            }
+          }}
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="nome"
+          placeholder="Nome"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="nomeApresentacao"
+          placeholder="Nome de Apresentação"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="telefone"
+          placeholder="Telefone"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="rg"
+          placeholder="RG"
+        />
+        <fieldset className="border border-gray-300 p-2 m-2 flex flex-col gap-2">
+          <legend className="text-sm font-bold">Endereço</legend>
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="rua"
+            placeholder="Rua"
           />
-          <input
-            type="text"
-            name="nome"
-            placeholder="Nome"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
-            type="text"
-            name="nomeApresentacao"
-            placeholder="Nome de Apresentação"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
-            type="text"
-            name="telefone"
-            placeholder="Telefone"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
-            type="text"
-            name="rg"
-            placeholder="RG"
-            className="border border-gray-300 p-2 m-2"
-          />
-
-          <fieldset className="border border-gray-300 p-2 m-2">
-            <legend className="text-sm font-bold">Endereço</legend>
-            <input
-              type="text"
-              name="rua"
-              placeholder="Rua"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="number"
-              name="numero"
-              placeholder="Número"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="text"
-              name="bairro"
-              placeholder="Bairro"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="text"
-              name="complemento"
-              placeholder="Complemento"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="text"
-              name="cep"
-              placeholder="CEP"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="text"
-              name="cidade"
-              placeholder="Cidade"
-              className="border border-gray-300 p-2 my-1"
-            />
-            <input
-              type="text"
-              name="estado"
-              placeholder="Estado"
-              className="border border-gray-300 p-2 my-1"
-            />
-          </fieldset>
-
-        </div>
-
-        <div className=" flex flex-col">
-          <input
-            type="text"
-            name="nomePai"
-            placeholder="Nome do Pai"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
-            type="text"
-            name="nomeMae"
-            placeholder="Nome da Mãe"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
-            type="date"
-            name="dataNascimento"
-            className="border border-gray-300 p-2 m-2"
-          />
-          <input
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="numero"
+            placeholder="Número"
             type="number"
-            name="peso"
-            placeholder="Peso (kg)"
-            className="border border-gray-300 p-2 m-2"
           />
-          <input
-            type="number"
-            name="altura"
-            placeholder="Altura (cm)"
-            className="border border-gray-300 p-2 m-2"
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="bairro"
+            placeholder="Bairro"
           />
-          <select
-            name="escolaridade"
-            className="border border-gray-300 p-2 m-2"
-            defaultValue="NÍVEL_MÉDIO"
-          >
-            <option value="NÍVEL_FUNDAMENTAL">Nível Fundamental</option>
-            <option value="NÍVEL_MÉDIO">Nível Médio</option>
-            <option value="NÍVEL_SUPERIOR">Nível Superior</option>
-          </select>
-          <input
-            type="text"
-            name="titulacao"
-            placeholder="Titulação"
-            className="border border-gray-300 p-2 m-2"
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="complemento"
+            placeholder="Complemento"
           />
-          <input
-            type="number"
-            name="tempoExperiencia"
-            placeholder="Tempo de Experiência"
-            className="border border-gray-300 p-2 m-2"
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="cep"
+            placeholder="CEP"
           />
-          <input
-            type="text"
-            name="habilidades"
-            placeholder="Habilidade"
-            className="border border-gray-300 p-2 m-2"
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="cidade"
+            placeholder="Cidade"
           />
-          <input
-            type="text"
-            name="experiencias"
-            placeholder="Experiência"
-            className="border border-gray-300 p-2 m-2"
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={control}
+            name="estado"
+            placeholder="Estado"
           />
-          <textarea
-            name="apresentacao"
-            placeholder="Apresentação"
-            className="border border-gray-300 p-2 m-2"
-          ></textarea>
+        </fieldset>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="nomePai"
+          placeholder="Nome do Pai"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="nomeMae"
+          placeholder="Nome da Mãe"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.DATE_PICKER}
+          control={control}
+          name="dataNascimento"
+          placeholder="Data de Nascimento"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="peso"
+          placeholder="Peso (kg)"
+          type="number"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="altura"
+          placeholder="Altura (cm)"
+          type="number"
+        />
 
-        </div>
-      </div>
+        <CustomFormField
+          fieldType={FormFieldType.SELECT}
+          control={control}
+          name="escolaridade"
+          placeholder="Escolaridade"
+        >
+          {Escolaridades.map((type, i) => (
+            <SelectItem key={type + i} value={type}>
+              {type}
+            </SelectItem>
+          ))}
+        </CustomFormField>
 
-      <button
-        type="submit"
-        className="bg-blue-500 text-white p-2 m-2 rounded hover:bg-blue-600"
-      >
-        Cadastrar Cuidador
-      </button>
-    </form>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="titulacao"
+          placeholder="Titulação"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="tempoExperiencia"
+          placeholder="Tempo de Experiência"
+          type="number"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="habilidades"
+          placeholder="Habilidades"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="experiencias"
+          placeholder="Experiências"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.TEXTAREA}
+          control={control}
+          name="apresentacao"
+          placeholder="Apresentação"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 m-2 rounded hover:bg-blue-600"
+        >
+          Cadastrar Cuidador
+        </button>
+      </form>
+    </FormProvider>
+
   );
 }

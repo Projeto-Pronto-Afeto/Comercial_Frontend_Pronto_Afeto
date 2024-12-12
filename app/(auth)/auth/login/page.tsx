6edc82bfe9fadd74a56a3cc297af1ab0039cd4f3
Rebeca Aguiar <beca.cout@gmail.com>
2024-12-12 16:52:42 -0300

    todo: fundo login alterado

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -4,17 +4,15 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="relative max-h-screen flex h-screen justify-end items-center bg-[#ebebeb] w-full p-4 ">
-      <div>
-        <Image
-          src={"/assets/images/shapes-img.png"}
-          width={1000}
-          height={1000}
-          className="w-fit hidden md:block  "
-          alt=""
-        />
-      </div>
-      <div className=" px-16 bg-white p-10  rounded-2xl h-full flex items-center  text-center">
+    <div className="relative max-h-screen flex h-screen justify-end items-center  w-full p-4 ">
+      <Image
+        src={"/assets/images/bg-gradient.jpg"}
+        fill
+        className="w-fit   "
+        alt=""
+      />
+
+      <div className="z-50 px-24 bg-white p-10  rounded-2xl h-full flex items-center  text-center">
         <LoginForm />
       </div>
     </div>

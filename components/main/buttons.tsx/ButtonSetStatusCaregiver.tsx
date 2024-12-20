4441d6c522cc0a19aba7d022c2144fcfa2529e65
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2024-12-20 11:54:45 -0300

    toast e useformstate

--- components/main/buttons.tsx/ButtonSetStatusCaregiver.tsx
+++ components/main/buttons.tsx/ButtonSetStatusCaregiver.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useFormState } from "react-dom";
 import { toast } from "sonner";
 import { setCuidadorStatus } from "@/actions/cuidador/cuidador.actions";
@@ -16,25 +16,20 @@ const ButtonSetStatusCaregiver = ({
 }) => {
   const initialState = { error: false, message: "", submitting: false };
 
-  // Gerenciar estado com `useFormState`
   const [formState, formAction] = useFormState(
     async () => {
       const response = await setCuidadorStatus(id, status);
+
+      if (!response.error) {
+        toast.success(response.message || "Status atualizado com sucesso!");
+      } else {
+        toast.error(response.message || "Erro ao atualizar status.");
+      }
+
       return response;
     },
     initialState
   );
-
-  // Exibir notificações de sucesso ou erro
-  useEffect(() => {
-    if (!formState.submitting) {
-      if (!formState.error) {
-        toast.success(formState.message || "Status atualizado com sucesso!");
-      } else {
-        toast.error(formState.message || "Erro ao atualizar status.");
-      }
-    }
-  }, [formState]);
 
   return (
     <form action={formAction}>
